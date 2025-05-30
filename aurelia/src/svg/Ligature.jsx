c67606c2b8ebc2c34e70ab0384a260e1b788c492
EvieePy <29671945+EvieePy@users.noreply.github.com>
2025-05-30 18:52:30 +1000

    Add some additional SVG

---
+++ aurelia/src/svg/Ligature.jsx
@@ -0,0 +1,17 @@
+const LigatureSVG = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    className="lucide lucide-ligature-icon lucide-ligature"
+    {...props}
+  >
+    <path d="M8 20V8c0-2.2 1.8-4 4-4 1.5 0 2.8.8 3.5 2M6 12h4M14 12h2v8M6 20h4M14 20h4" />
+  </svg>
+)
+export default LigatureSVG
