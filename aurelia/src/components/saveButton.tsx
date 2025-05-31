303c015c20644f76b0d38f7164b3ca5c0ac13052
EvieePy <29671945+EvieePy@users.noreply.github.com>
2025-05-31 23:27:04 +1000

    Add SaveButton, new SVGs and CSS Changes

---
+++ aurelia/src/components/saveButton.tsx
@@ -0,0 +1,32 @@
+import SettingsSVG from "~/svg/Settings";
+import SettingsModal from "./settingsModal";
+import { createSignal } from "solid-js";
+import { usePasteContext } from "~/stores/paste";
+
+interface Props {
+    checked: boolean;
+}
+
+export default function SaveButton() {
+    const [showModal, setShowModal] = createSignal(false);
+
+    const handleSettingsModal = (e: MouseEvent) => {
+        e.stopPropagation();
+        setShowModal(false);
+    }
+
+    const [pasteStore, setPasteStore] = usePasteContext();
+
+    return (
+        <div class="saveContainer">
+            <SettingsModal showModal={showModal()} title="Save Options" onOutsideClick={handleSettingsModal}/>
+            
+            <div class="saveButton">
+                Save Paste
+            </div>
+            <div class="saveSettings" onclick={() => setShowModal(true)}>
+                <SettingsSVG />
+            </div>
+        </div>
+    )
+}
---
+++ aurelia/src/svg/Home.jsx
@@ -0,0 +1,13 @@
+const HomeSVG = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="currentColor"
+    viewBox="0 -960 960 960"
+    {...props}
+  >
+    <path d="M160-120v-480l320-240 320 240v480H560v-280H400v280H160Z" />
+  </svg>
+)
+export default HomeSVG
